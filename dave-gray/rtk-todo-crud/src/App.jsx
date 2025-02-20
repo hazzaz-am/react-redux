17319a3bdea08ae7ea20d4b5c3ec07ed402f223a
hazzaz-am <hazzazabdulmannan@gmail.com>
2025-02-20 14:28:06 +0600

    start new project

---
+++ dave-gray/rtk-todo-crud/src/App.jsx
@@ -0,0 +1,10 @@
+import TodoList from "./features/todos/TodoList";
+
+const App = () => {
+	return (
+		<>
+			<TodoList />
+		</>
+	);
+};
+export default App;
---
+++ dave-gray/rtk-todo-crud/src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+
+export const store = configureStore({
+  reducer: {
+    // Define a reducer here
+  },
+})
