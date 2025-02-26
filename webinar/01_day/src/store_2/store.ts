a27b987e15c61c573b2f89923c1ec717e0917c7b
hazzaz-am <hazzazabdulmannan@gmail.com>
2025-02-26 22:28:22 +0600

    start new project with rtk query

--- webinar/01_day/src/store_2/store.ts
+++ webinar/01_day/src/store_2/store.ts
@@ -1,19 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { movieReducer } from "./reducers/movieReducer";
-import  cartReducer from "./reducers/cartReducer";
+import cartReducer from "./reducers/cartReducer";
 
 export const store = configureStore({
 	reducer: {
 		movies: movieReducer,
-    cart: cartReducer
+		cart: cartReducer,
 	},
-	// preloadedState: {
-	// 	movies: {
-	// 		movies: [],
-	// 		isLoading: true,
-	// 		error: "Nice try",
-	// 	},
-	// },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
+
+// preloadedState: {
+// 	movies: {
+// 		movies: [],
+// 		isLoading: true,
+// 		error: "Nice try",
+// 	},
+// },
